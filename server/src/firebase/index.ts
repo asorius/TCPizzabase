b05e5f19afcfe8a6d8ac1e60434239001a08722a
asorius <18assdd@gmail.com>
2024-04-19 16:40:02 +0100

    bugfix: check for file import at firebasekey json file

--- server/src/firebase/index.ts
+++ server/src/firebase/index.ts
@@ -1,11 +1,20 @@
 import { initializeApp, cert, type ServiceAccount } from 'firebase-admin/app'
 import { getStorage } from 'firebase-admin/storage'
 import config from '@server/config'
-import serviceAccount from './serviceAccountKey.json'
+
+const loadService = async () => {
+  try {
+    // @ts-ignore
+    return await import('./serviceAccountKey.json')
+  } catch (error) {
+    return false
+  }
+}
 
 let app = null
+const serviceAccount = await loadService()
 
-if (config.firebase.bucket) {
+if (config.firebase.bucket && serviceAccount) {
   app = initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
     storageBucket: config.firebase.bucket,
