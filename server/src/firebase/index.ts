e580e5be124fa7ad7688d46d9f8b9ee24cc5a230
asorius <18assdd@gmail.com>
2024-04-24 11:37:08 +0100

    fs to env

--- server/src/firebase/index.ts
+++ server/src/firebase/index.ts
@@ -7,7 +7,21 @@ const loadService = async () => {
     // @ts-ignore
     return await import('./serviceAccountKey.json')
   } catch (error) {
-    return false
+    const serviceAccount = {
+      type: 'service_account',
+      project_id: process.env.FS_PROJECT_ID,
+      private_key_id: process.env.FS_PRIVATE_KEY_ID,
+      // See: https://stackoverflow.com/a/50376092/3403247.
+      private_key: (process.env.FS_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
+      client_email: process.env.FS_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FS_ADMIN_CLIENT_ID,
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: process.env.FS_CERT_URL,
+      universe_domain: 'googleapis.com',
+    } as ServiceAccount
+    return serviceAccount
   }
 }
 
